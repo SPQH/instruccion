bd4931cf80762bb190db04b5b3121c95c5bd7aee
SPQH <albertofb@pm.me>
2020-04-15 19:09:43 +0200

    He creado mi primer Hola Mundo en python

---
+++ holamundo.py
@@ -0,0 +1,10 @@
+nombre=input("Dime tu nombre: ")
+print("Hola mundo, " + nombre + " capullo")
+
+edad=input("¿Cuantos años tienes?: ")
+print("Tienes " + edad + " años")
+
+resta=100-int(edad) # int (integer) es número entero / float (floating) es número flotante (decimal)
+print("Hola " + nombre + " tienes " + edad + " años y te faltan " + str(resta) + " para llegar a los 100") # str (string) es texto
+
+#nombre=input("¿Eres hereje?")
