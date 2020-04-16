ba3b7a6ce2f65f1ec5986384c97b9d9d343658bd
SPQH <albertofb@pm.me>
2020-04-16 20:58:25 +0200

    Avances

--- python/prueba.py
+++ python/prueba.py
@@ -7,11 +7,14 @@ direccion= "Calle Reyes Catolicos"
 ntest= 342221
 nombre=input("Nombre completo: ")
 direccion=input("Domicilio: ")
-ntest=input("Identificador de la prueba: ")
+ntest=int(input("Identificador de la prueba: "))
 
 if nombre != "Alberto Balon Salado" and direccion != "Calle Reyes Catolicos" and ntest != 342221:
     print("Sus datos no se corresponden con los asociados a este documento. Por favor, intentelo de nuevo.")
+elif nombre == "Alberto Balon Salado" or direccion == "Calle Reyes Catolicos" or ntest == 342221:
+    print("dfsdfsdfsdfsd")
 else:
     print("Hola Alberto, has accedido correctamente. Su prueba ha resultado positiva por COVID.")
 
+    print("Hola")
 
