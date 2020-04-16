c6f01a8b5d77938be6b3b4cb6926a9163144b367
SPQH <albertofb@pm.me>
2020-04-16 12:20:50 +0200

    He añadido varias carpetas

---
+++ prueba.py
@@ -0,0 +1,17 @@
+
+print("RESULTADOS TEST CORONAVIRUS")
+print("Por favor, confirme sus datos personales para acceder a los resultados del test: ")
+
+nombre= "Alberto Balon Salado"
+direccion= "Calle Reyes Catolicos"
+ntest= 342221
+nombre=input("Nombre completo: ")
+direccion=input("Domicilio: ")
+ntest=input("Identificador de la prueba: ")
+
+if nombre != "Alberto Balon Salado" and direccion != "Calle Reyes Catolicos" and ntest != 342221:
+    print("Sus datos no se corresponden con los asociados a este documento. Por favor, intentelo de nuevo.")
+else:
+    print("Hola Alberto, has accedido correctamente. Su prueba ha resultado positiva por COVID.")
+
+
