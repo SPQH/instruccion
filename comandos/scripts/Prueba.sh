c41626f002c98b549e9043e395852a989e9ad92d
SPQH <albertofb@pm.me>
2020-05-02 18:34:50 +0200

    Ultimos cambios en latex

--- comandos/scripts/Prueba.sh
+++ comandos/scripts/Prueba.sh
@@ -3,5 +3,5 @@
 time=$(date '+%d/%m/%Y %H:%M:%S')
 
 echo -e "INICIO SCRIPT \t ($time)" >> ifc.log
-ifconfig >> ifc.log
+/sbin/ifconfig >> /tmp/ifc.log
 echo -e "FIN SCRIPT \t ($time) \n \n" >> ifc.log
